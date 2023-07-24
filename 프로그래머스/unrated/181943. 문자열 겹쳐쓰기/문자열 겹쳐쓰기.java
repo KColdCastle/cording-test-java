a6b52e05257cbcbd3d2818dddd4bb4999178ad9c
KColdCastle <139545813+KColdCastle@users.noreply.github.com>
2023-07-24 17:59:37 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.36 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        try {
+            StringBuffer sb = new StringBuffer(my_string);
+            // sb.replace(s, 12, overwrite_string);
+            answer = sb
+                    .replace(s, my_string.length() - (my_string.substring(s).length() - overwrite_string.length()),
+                            overwrite_string)
+                    .toString();
+            System.out.println(answer);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
+        return answer;
+    }
+}
