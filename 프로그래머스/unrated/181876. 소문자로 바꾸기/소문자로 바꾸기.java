d014e474fb89cf295ddfd05d5387f1d26bc92290
KColdCastle <139545813+KColdCastle@users.noreply.github.com>
2023-07-24 17:56:51 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.24 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        answer = myString.toLowerCase();
+        System.out.println(answer);
+        return answer;
+    }
+}
